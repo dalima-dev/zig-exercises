8c3dee97877cdacda0728eb800a4678a07459a9b
Daniel Alves <dalima.dev@gmail.com>
2024-12-29 21:40:56 -0300

    solve 072

--- exercises/072_comptime7.zig
+++ exercises/072_comptime7.zig
@@ -35,11 +35,13 @@ pub fn main() void {
     // at compile time.
     //
     // Please fix this to loop once per "instruction":
-    ??? (i < instructions.len) : (???) {
+    inline while (i < instructions.len) : (i += 3) {
 
         // This gets the digit from the "instruction". Can you
         // figure out why we subtract '0' from it?
         const digit = instructions[i + 1] - '0';
+        // The character '0' has integer value of 48, so it's subtracted from instructions[i + 1].
+        // @compileLog(instructions[i + 1], digit, '0');
 
         // This 'switch' statement contains the actual work done
         // at runtime. At first, this doesn't seem exciting...
