206a369235047c53ae9389433003b2462ec97448
Daniel <dalima.dev@gmail.com>
2024-12-30 17:45:57 -0300

    solve 092

--- exercises/092_interfaces.zig
+++ exercises/092_interfaces.zig
@@ -104,9 +104,9 @@ pub fn main() !void {
 
     std.debug.print("Daily Insect Report:\n", .{});
     for (my_insects) |insect| {
-        // Almost done! We want to print() each insect with a
+        // Almost done! We want to print() einsectach insect with a
         // single method call here.
-        ???
+        insect.print();
     }
 }
 
