c08a26343b35b1c12e95db068e18f6233b70e7c6
Daniel Alves <dalima.dev@gmail.com>
2025-01-11 16:04:47 -0300

    solve 100

--- exercises/100_for4.zig
+++ exercises/100_for4.zig
@@ -39,7 +39,7 @@ pub fn main() void {
     const hex_nums = [_]u8{ 0xb, 0x2a, 0x77 };
     const dec_nums = [_]u8{ 11, 42, 119 };
 
-    for (hex_nums, ???) |hn, ???| {
+    for (hex_nums, dec_nums) |hn, dn| {
         if (hn != dn) {
             std.debug.print("Uh oh! Found a mismatch: {d} vs {d}\n", .{ hn, dn });
             return;
@@ -55,8 +55,8 @@ pub fn main() void {
 // By the way, congratulations for making it to Exercise 100!
 //
 //    +-------------+
-//    | Celebration |
-//    | Area  * * * |
+//    | Happy new   |
+//    |   year!     |
 //    +-------------+
 //
 // Please keep your celebrating within the area provided.
